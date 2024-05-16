3d2f34665de4394cf943af4db962e7e5a4ff25a5
Johnny Bouder <jbouder@metrostar.com>
2024-05-16 10:18:21 -0400

    Add docker script.

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,6 @@
 # Find and Kill Process
 lsof -i tcp:8000
 kill -9 12345
+
+# Stop all Docker containers matching a name
+docker container stop $(docker container ls -q --filter name=code-migration-test-env)  
